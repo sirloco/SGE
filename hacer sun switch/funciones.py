d9dfc6848f2678928d9c26f2a01f73d25e6515c0
santi <sirlocus66@gmail.com>
2019-12-04 21:42:41 +0100

    select hecho

--- hacer sun switch/funciones.py
+++ hacer sun switch/funciones.py
@@ -1,9 +1,29 @@
-def multiplicar():
-    print ("tabla")
+def multiplicar(n1,n2):
+    print(n1, "X", n2, "=", n1*n2)
 
-funciones = {1: multiplicar}
+def sumar(n1,n2):
+    print(n1, "+", n2, "=", n1+n2)
 
-opcion = int(input("opcion: "))
+def resta(n1,n2):
+    print(n1, "-", n2, "=", n1-n2)
 
-funciones.get(opcion)()
+def division(n1,n2):
+    print(n1, "/", n2, "=", n1/n2)
+
+funciones = {1: multiplicar,2: sumar,3: resta,4: division}
+
+opcion = -1
+while opcion != 5:
+    n1 = int(input("Escribe un número: "))
+    n2 = int(input("Escribe otro número: "))
+
+    print("1.- multiplica\n"
+        "2.- suma\n"
+        "3.- resta\n"
+        "4.- divide\n"
+        "5.- salir")
+    opcion = int(input("opcion: "))
+
+    if opcion !=5:
+        funciones.get(opcion)(n1, n2)
 
